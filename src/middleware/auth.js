40c1eb31e4e30cd8ba10912a7e6d7ff3f63a8538
Ramesh Rs <rameshrs@centizen.com>
2024-08-25 10:07:37 +0530

    remove console

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const API_TOKEN = process.env.API_TOKEN;
 
 const authenticateToken = async function name(req , res , next) {
-    console.log("djwbd",req.headers);
     const token = req.headers['authorization'];
     if(!token || token != API_TOKEN){
         return res.status(401).json({error : 'unauthorized'});
